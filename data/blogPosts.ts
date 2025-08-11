3fa37426e576761b4fa7e7189832d2a8e688fed7
jpoulain58 <jpoulain58@gmail.com>
2025-08-11 03:07:28 +0200

    Test notif #9: relance workflow (envoi unique attendu)

--- data/blogPosts.ts
+++ data/blogPosts.ts
@@ -31,4 +31,4 @@ export const blogPosts: BlogPost[] = [
 ]
 
 
-// Changement mineur pour déclencher la notification automatique (test envoi #8)
+// Changement mineur pour déclencher la notification automatique (test envoi #9)
